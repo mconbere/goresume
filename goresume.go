278e0f4aee629094edbbd7fd484b8dfdfd546863
Morgan Conbere <mconbere@gmail.com>
2015-10-20 16:09:32 -0700

    new resume version, little more robust

--- goresume.go
+++ goresume.go
@@ -1,8 +1,20 @@
 package goresume
 
 import (
+	"io"
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"strconv"
+	"io/ioutil"
+	"net/url"
+
+	"github.com/golang/protobuf/proto"
+	md "github.com/russross/blackfriday"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+
+	resumepb "proto"
 )
 
 func init() {
@@ -10,5 +22,161 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Resume!")
+	ctx := appengine.NewContext(r)
+
+	url, data, text, output, err := parseURL(r.URL)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if url != nil {
+		client := urlfetch.Client(ctx)
+		resp, err := client.Get(url.String())
+		defer resp.Body.Close()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		data, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	}
+
+	resume, err := func() (*resumepb.Resume, error) {
+		r := &resumepb.Resume{}
+		if text == true {
+			err := proto.UnmarshalText(string(data), r)
+			if err != nil {
+				return nil, fmt.Errorf("Could not unmarshal proto text: %v", err)
+			}
+			return r, nil
+		}
+		err := proto.Unmarshal(data, r)
+		if err != nil {
+			return nil, fmt.Errorf("Could not unmarshal proto data: %v", err)
+		}
+		return r, nil
+	}()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := output(w, resume); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
+
+func outputBinary(w http.ResponseWriter, r *resumepb.Resume) error {
+	w.Header().Add("Content-Type", "application/octet-stream")
+	b, err := proto.Marshal(r)
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(b)
+	return err
+}
+
+func outputText(w http.ResponseWriter, r *resumepb.Resume) error {
+	w.Header().Add("Content-Type", "text/plain")
+	return proto.MarshalText(w, r)
+}
+
+func outputMarkdown(w http.ResponseWriter, r *resumepb.Resume) error {
+	s := resumeToMarkdown(r)
+	_, err := io.WriteString(w, s)
+	return err
+}
+
+func outputHTML(w http.ResponseWriter, r *resumepb.Resume) error {
+	s := resumeToMarkdown(r)
+	b := md.MarkdownCommon([]byte(s))
+	_, err := w.Write(b)
+	return err
+}
+
+const (
+	queryURL    string = "url"
+	queryData   string = "data"
+	queryText   string = "text"
+	queryOutput string = "output"
+)
+
+type outputFunc func(http.ResponseWriter, *resumepb.Resume) error
+
+var outputMap = map[string]outputFunc{
+	"binary": outputBinary,
+	"text": outputText,
+	"markdown": outputMarkdown,
+	"html": outputHTML,
+}
+var outputDefault = outputText
+
+const textDefault bool = false
+
+func parseURL(in *url.URL) (u *url.URL, data []byte, text bool, output outputFunc, err error) {
+	values := in.Query()
+
+	u, err = func() (*url.URL, error) {
+		v := values.Get(queryURL)
+		if v == "" {
+			return nil, nil
+		}
+		return url.Parse(v)
+	}()
+	if err != nil {
+		return
+	}
+
+	data, err = func() ([]byte, error) {
+		v := values.Get(queryData)
+		if v == "" {
+			return nil, nil
+		}
+		return base64.StdEncoding.DecodeString(v)
+	}()
+	if err != nil {
+		return
+	}
+
+	text, err = func() (bool, error) {
+		v := values.Get(queryText)
+		if v == "" {
+			return textDefault, nil
+		}
+		return strconv.ParseBool(v)
+	}()
+	if err != nil {
+		return
+	}
+
+	output, err = func() (outputFunc, error) {
+		v := values.Get(queryOutput)
+		if v == "" {
+			return outputDefault, nil
+		}
+		o, ok := outputMap[v]
+		if !ok {
+			return nil, fmt.Errorf("unknown output %q", v)
+		}
+		return o, nil
+	}()
+	if err != nil {
+		return
+	}
+
+	if u == nil && data == nil {
+		err = fmt.Errorf("Query must contain %q or %q value", queryURL, queryData)
+		return
+	}
+
+	if u != nil && data != nil {
+		err = fmt.Errorf("Query must not contain both %q and %q value", queryURL, queryData)
+		return
+	}
+	return
+} 
---
+++ markdown.go
@@ -0,0 +1,159 @@
+package goresume
+
+import (
+  "strconv"
+	"strings"
+
+  pb "proto"
+)
+
+const (
+	nl = "\n"
+	nlnl = "\n\n"
+	br = "<br>\n"
+)
+
+func resumeToMarkdown(r *pb.Resume) (out string) {
+	// Name as title:
+	if r.Name != "" {
+		out += title("=", r.Name)
+	}
+
+	// Header info:
+	if r.PhoneNumber != "" {
+		out += r.PhoneNumber + br
+	}
+	if r.EmailAddress != "" {
+		out += mailto(r.EmailAddress) + br
+	}
+	if r.Website != "" {
+		out += web(r.Website) + br
+	}
+	if r.Address != nil {
+		out += strings.Join(r.Address.Line, ", ")
+	}
+	out += br + nl
+
+	if r.Objective != nil {
+		o := r.Objective
+		oTitle := o.Title
+		if oTitle == "" {
+			oTitle = "Objective"
+		}
+		out += title("-", oTitle)
+		out += o.Contents + nlnl
+	}
+
+	if len(r.Education) > 0 {
+		out += title("-", "Education")
+		for _, e := range r.Education {
+			out += bold(e.Institute) + br
+			out += e.Degree + br
+			out += dateRange(e.DateRange) + br
+			out += bulletList(e.Note) + nl
+		}
+	}
+
+	if len(r.Employment) > 0 {
+		out += title("-", "Employment")
+		for _, e := range r.Employment {
+			out += bold(e.Company) + br
+			out += e.Title + br
+			out += dateRange(e.DateRange) + br
+			out += bulletList(e.Note) + nl
+		}
+	}
+
+	if len(r.Project) > 0 {
+		out += title("-", "Projects")
+		for _, p := range r.Project {
+			out += bold(p.Title) + br
+			out += p.Role + br
+			out += dateRange(p.DateRange) + br
+			if p.Website != "" {
+				out += web(p.Website) + br
+			}
+			out += bulletList(p.Note) + nl
+		}
+	}
+
+	if len(r.Publication) > 0 {
+		out += title("-", "Publications")
+		for _, p := range r.Publication {
+			out += bold(p.Title) + br
+			out += andList(p.Author) + br
+			if p.Date != nil {
+				out += date(p.Date) + br
+			}
+			if p.Url != "" {
+				out += web(p.Url) + br
+			}
+			out += nl
+			out += bulletList(p.Note)
+		}
+	}
+	return
+}
+
+func title(sep, t string) (out string) {
+	out += t + nl
+	out += strings.Repeat(sep, len(t)) + nlnl
+	return
+}
+
+func bulletList(l []string) (out string) {
+	out += nl
+	for _, e := range l {
+		out += "*   " + e + nl
+	}
+	return
+}
+
+func andList(l []string) string {
+	if len(l) == 0 {
+		return ""
+	} else if len(l) == 1 {
+		return l[0]
+	}
+	return strings.Join(l[:len(l)-2], ", ") + " and " + l[len(l)-1]
+}
+
+func date(d *pb.Date) string {
+	if d == nil || (d.Year == 0 && d.Month == pb.Date_UNKNOWN) {
+		return "Present"
+	}
+	var s string
+	if d.Month != pb.Date_UNKNOWN {
+		s += strings.Title(strings.ToLower(d.Month.String()))
+		if d.Day != 0 {
+			s += " " + strconv.FormatUint(uint64(d.Day), 10)
+		}
+	}
+	if d.Year != 0 {
+		if len(s) > 0 {
+			s += " "
+		}
+		s += strconv.FormatUint(uint64(d.Year), 10)
+	}
+	return s
+}
+
+func dateRange(dr *pb.DateRange) string {
+	return date(dr.Began) + " \u2013 " + date(dr.Ended)
+}
+
+func code(s string) string {
+	return "`" + s + "`"
+}
+
+func bold(s string) string {
+	return "__" + s + "__"
+}
+
+func mailto(e string) string {
+	return "[" + code(e) + "](mailto:" + e + ")" 
+}
+	
+func web(u string) string {
+	return "[" + code(u) + "](" + u + ")"
+}
